0102e3dc2a2c70b74a1bb95944cd09441412f53a
nihal1993 <jaimini.nihal@gmail.com>
2019-11-14 05:08:06 +0530

    Updates

---
+++ precache-manifest.98cfa6022f8649abbde8742d2bc5ae26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1223a056c967da0fec6e02ea73d62588",
+    "url": "/my-apps/index.html"
+  },
+  {
+    "revision": "9e85e420db388646f5de",
+    "url": "/my-apps/static/css/main.e35898c3.chunk.css"
+  },
+  {
+    "revision": "539d5485135010369838",
+    "url": "/my-apps/static/js/2.00b975d2.chunk.js"
+  },
+  {
+    "revision": "9e85e420db388646f5de",
+    "url": "/my-apps/static/js/main.fffef3c0.chunk.js"
+  },
+  {
+    "revision": "cfb26456184f1dcd8324",
+    "url": "/my-apps/static/js/runtime-main.8287696a.js"
+  }
+]);
